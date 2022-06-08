efa6882c0c0b9ad2e32ae63295a52a04f91f05a4
Marco Qin <tqin@brandeis.edu>
2022-06-08 15:56:39 -0400

    minor changes

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
+import { Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+
+// is this how you declare?
+var Root = createNativeStackNavigator
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Root.Navigator>
+        <Root.Screen name={"About"} component={About}/>
+        <Root.Screen name={"Item Detail"} component={ItemDetail}/>
+      </Root.Navigator>
+     
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+
+    </NavigationContainer>
+    
   );
 }
 
